c8375150214069fcc30e5ff8b6c5f8f135423023
duckyduckG <duckyduckg65@gmail.com>
2025-10-03 16:55:46 +0500

    usb: gadget: function: f_mtp: add missed change
    * from vayu-r-oss

--- drivers/usb/gadget/function/f_mtp.c
+++ drivers/usb/gadget/function/f_mtp.c
@@ -617,6 +617,9 @@ static ssize_t mtp_read(struct file *fp, char __user *buf,
 		goto wait_err;
 	}
 
+#if defined(CONFIG_MACH_XIAOMI_VAYU)
+	cdev = dev->cdev;
+#endif
 	len = ALIGN(count, dev->ep_out->maxpacket);
 	if (len > mtp_rx_req_len)
 		return -EINVAL;

05dbfbb801677d28044f7d006cfe74b5545f42d7
duckyduckG <duckyduckg65@gmail.com>
2025-10-21 00:59:35 +0500

    techpack: display: dsi: drop panel_mode checks
    it conflicts with fp_quickon

--- techpack/display/msm/dsi/dsi_drm.c
+++ techpack/display/msm/dsi/dsi_drm.c
@@ -245,17 +245,9 @@ static void dsi_bridge_pre_enable(struct drm_bridge *bridge)
 			drm_notifier_call_chain(DRM_EARLY_EVENT_BLANK, &g_notify_data);
 			drm_notifier_call_chain(DRM_EVENT_BLANK, &g_notify_data);
 			dev->fp_quickon = false;
-			return;
-		} else if (c_bridge->display->panel->panel_mode == DSI_OP_VIDEO_MODE) {
-			DSI_DEBUG("skip set display config for video panel in fpc\n");
-			return;
-		} else if (c_bridge->display->panel->panel_mode == DSI_OP_CMD_MODE &&
-		    c_bridge->dsi_mode.dsi_mode_flags != DSI_MODE_FLAG_DMS) {
-			DSI_DEBUG("skip set display config because timming not switch for command panel\n");
-
+		}
 		DSI_DEBUG("%s panel already on\n", __func__);
 		return;
-		}
 	}
 
 	drm_notifier_call_chain(DRM_EARLY_EVENT_BLANK, &g_notify_data);
